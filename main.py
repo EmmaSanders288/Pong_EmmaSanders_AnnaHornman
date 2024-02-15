3915a41c2149d12249025f5004ec90eb5387d501
Emma-Sanders <326358@annavanrijn.nl>
2024-02-15 11:08:20 +0100

    version 15-2-2024 11.07

--- main.py
+++ main.py
@@ -1,3 +1,17 @@
 import pygame
 
+class Pong:
+
+    def __init__(self):
+        pass
+
+    def game_loop(self):
+        pass
+
+def main():
+    pong = Pong()
+    pong.game_loop()
+
+if __name__ == "__main__":
+    main()
 
